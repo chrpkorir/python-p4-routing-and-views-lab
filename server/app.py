22e12180b7cb964a24de6c9e48ac42bf951d6bd1
chrpkorir <faythkoko@gmail.com>
2025-03-30 14:15:00 +0300

    Practised working on views

--- server/app.py
+++ server/app.py
@@ -4,5 +4,42 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+@app.route('/print/<string:text>')
+def print_string(text):
+    print(text) #  print the string in the console
+    return text # Display it in the web browser
+
+@app.route('/count/<int:num>')
+def count(num):
+    numbers = [str(i) for i in range(num + 1)] 
+    return "<br>".join(numbers)
+
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    result = None
+    if operation == "+":
+        result = num1 + num2
+    elif operation == "-":
+        result = num1 - num2
+    elif operation == "div" and num2 != 0:
+        result = num1 / num2
+    elif operation == "*":
+        result = num1 * num2
+    elif operation == "%" and num2 != 0:
+        result = num1 % num2
+    
+    if result is not None:
+        
+        return str(result)
+    else:
+        return "Error: Invalid operation"
+
+    
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
